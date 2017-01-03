b19a2df056b3cf38f2f7b829cd45c063187bfc19
Thomas Kölling <spamdoggi@gmail.com>
2017-01-03 11:21:32 +0100

    add empty list response

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 
+// services
+const list = require('./service/list')
+
+
 app.set('port', (process.env.PORT || 5000))
 
-app.get('/', (request, response) => {
-  response.json({status: 'ok'})
+app.get('/', (req, res) => {
+  res.json({status: 'ok'})
+})
+
+app.get('/list', (req, res) => {
+  res.json(list())
 })
 
 app.listen(app.get('port'), () => {
