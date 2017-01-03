63077402b21c880326a1772dad4fd64804d4ab5e
Thomas Kölling <spamdoggi@gmail.com>
2017-01-03 13:56:49 +0100

    initial msbot support

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
+const builder = require('botbuilder')
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -13,6 +15,18 @@ const list = require('./service/list')
 const take = require('./service/take')
 
 app.set('port', (process.env.PORT || 5000))
+
+// Create chat bot
+const connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+})
+
+
+const bot = new builder.UniversalBot(connector)
+
+app.post('/api/messages', connector.listen())
+
 
 app.get('/', (req, res) => {
   res.json({status: 'ok'})
@@ -26,11 +40,6 @@ app.get('/list', (req, res) => {
 app.post('/take/:vm', (req, res) => {
   const takeService = take(datasource)
   res.json(takeService.take(req.params.vm))
-})
-
-app.post('/messaging', (req, res) => {
-  console.log(req.body)
-  res.json(req.body)
 })
 
 app.listen(app.get('port'), () => {
