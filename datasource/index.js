cbf0d1e67686560f0919a0b57b3fe727a15b9166
Thomas Kölling <spamdoggi@gmail.com>
2017-01-03 12:03:06 +0100

    replace dummy list with an inmemory datasource

--- datasource/index.js
+++ datasource/index.js
@@ -1,5 +1,20 @@
 const rawlist = require('./list')
 
-module.exports = {
-  get: () => rawlist
+class ListEntry {
+  constructor(vm, user) {
+    this.name = vm
+    this.user = user
+  }
 }
+
+class InMemorySource {
+  constructor(initalList = rawlist) {
+    this.vmList = initalList.map(vmName => new ListEntry(vmName))
+  }
+
+  get() {
+    return this.vmList
+  }
+}
+
+module.exports = new InMemorySource()
